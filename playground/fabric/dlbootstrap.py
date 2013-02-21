0f9c7ec3e0b9a178c03733bbf0c82a96db3b4ed8
Nils Toedtmann <github.com-mail@nils.toedtmann.net>
2013-02-21 03:36:42 +0100

    Split dlbootstrap() into stage1 and stage2, added regenerate_ssh_host_keys

--- playground/fabric/dlbootstrap.py
+++ playground/fabric/dlbootstrap.py
@@ -1,6 +1,6 @@
 
 import nilsson
-from fabric.api import settings, sudo, env # run,  task
+from fabric.api import settings, sudo, env, run # task
 
 root_keys=['nils.toedtmann']
 admin_keys=['nils.toedtmann','joe.short','dan.mauger']
@@ -11,17 +11,27 @@ admin_group='adm'
 #   Bug: when call as 'admin', the root key is appended again
 #   configure postfix
 #   profiles, e.g. password hash
-#   Copy over user skeletton
 #   in CentOS: install man
+#   install & configure ufw
 
 
-def dlbootstrap(hostname):
+def dlbootstrap_stage1(hostname):
+    '''
+    Phase I of DL customization. To be run as 'root'
+    '''
+    if nilsson.am_not_root():
+        raise Exception('FATAL: must be root')
+
     if not hostname or hostname == 'None':
         hostname = env.host
 
+    nilsson.set_hostname(hostname)
+    nilsson.regenerate_ssh_host_keys()
+
+    # Customize root account
     for key in root_keys:
         nilsson.ssh_add_public_key(key, user='root')
-    nilsson.push_skeleton(local_path=./files/home-skel/,remote_path=.)
+    nilsson.push_skeleton(local_path='./files/home-skel/',remote_path='.')
 
     nilsson.allow_sudo_group()
 
@@ -30,18 +40,28 @@ def dlbootstrap(hostname):
     for key in admin_keys:
         nilsson.ssh_add_public_key(key, user=admin_user)
 
-    with settings(user=admin_user):
-        nilsson.push_skeleton(local_path=./files/home-skel/,remote_path=.)
-        nilsson.set_hostname(hostname)
-        nilsson.harden_sshd()
-        nilsson.lock_user('root')
-        nilsson.pkg_upgrade()
+    print 'Set new password for user "%s": ' % admin_user
+    run('passwd %s' % admin_user)
 
-        packages = ['postfix', 'screen', 'man', 'vim']
-        if nilsson.distro_flavour() == 'debian':
-            packages.append('bsd-mailx')
-        elif nilsson.distro_flavour() == 'redhat':
-            packages.append('mailx')
-        nilsson.pkg_install(packages)
+
+def dlbootstrap_stage2():
+    '''
+    Phase II of DL customization. To be executed as 'admin'
+    '''
+    nilsson.push_skeleton(local_path='./files/home-skel/', remote_path='.')
+
+    # Test we can sudo before we proceed!
+    sudo('id')
+
+    nilsson.harden_sshd()
+    nilsson.lock_user('root')
+    nilsson.pkg_upgrade()
+
+    packages = ['postfix', 'screen', 'man', 'vim']
+    if nilsson.distro_flavour() == 'debian':
+        packages.append('bsd-mailx')
+    elif nilsson.distro_flavour() == 'redhat':
+        packages.append('mailx')
+    nilsson.pkg_install(packages)
 
         
